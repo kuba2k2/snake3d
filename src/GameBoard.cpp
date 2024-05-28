e3787ba3bd2fe1967699222ec086aab145c31fb3
Kuba Szczodrzyński <kuba@szczodrzynski.pl>
2024-05-28 10:28:52 +0200

    Move game board down

--- src/GameBoard.cpp
+++ src/GameBoard.cpp
@@ -19,29 +19,30 @@ GameBoard::~GameBoard() {
 
 void GameBoard::draw(GLFWwindow *window, glm::mat4 P, glm::mat4 V) {
 	glm::mat4 M;
+	glm::vec3 offset = glm::vec3(0.0f, -1.5f, 0.0f);
 
 	// floor
-	M = glm::mat4(1.0f);
+	M = glm::translate(glm::mat4(1.0f), offset);
 	M = glm::scale(M, glm::vec3(this->length, this->floor, this->width));
 	this->modelSolid->draw(window, ShaderProgramType::SP_LAMBERT, P, V, M);
 
 	// wall Z+
-	M = glm::mat4(1.0f);
+	M = glm::translate(glm::mat4(1.0f), offset);
 	M = glm::translate(M, glm::vec3(0.0f, this->height / 2.0f, this->width / 2.0f - this->wall / 2.0f));
 	M = glm::scale(M, glm::vec3(this->length, this->height, this->wall));
 	this->modelSolid->draw(window, ShaderProgramType::SP_LAMBERT, P, V, M);
 	// wall Z-
-	M = glm::mat4(1.0f);
+	M = glm::translate(glm::mat4(1.0f), offset);
 	M = glm::translate(M, glm::vec3(0.0f, this->height / 2.0f, -(this->width / 2.0f - this->wall / 2.0f)));
 	M = glm::scale(M, glm::vec3(this->length, this->height, this->wall));
 	this->modelSolid->draw(window, ShaderProgramType::SP_LAMBERT, P, V, M);
 	// wall X+
-	M = glm::mat4(1.0f);
+	M = glm::translate(glm::mat4(1.0f), offset);
 	M = glm::translate(M, glm::vec3(this->length / 2.0f - this->wall / 2.0f, this->height / 2.0f, 0.0f));
 	M = glm::scale(M, glm::vec3(this->wall, this->height, this->width));
 	this->modelSolid->draw(window, ShaderProgramType::SP_LAMBERT, P, V, M);
 	// wall X-
-	M = glm::mat4(1.0f);
+	M = glm::translate(glm::mat4(1.0f), offset);
 	M = glm::translate(M, glm::vec3(-(this->length / 2.0f - this->wall / 2.0f), this->height / 2.0f, 0.0f));
 	M = glm::scale(M, glm::vec3(this->wall, this->height, this->width));
 	this->modelSolid->draw(window, ShaderProgramType::SP_LAMBERT, P, V, M);
@@ -55,7 +56,7 @@ void GameBoard::draw(GLFWwindow *window, glm::mat4 P, glm::mat4 V) {
 	float thickness = 10.0f;
 
 	// floor
-	M							= glm::mat4(1.0f);
+	M							= glm::translate(glm::mat4(1.0f), offset);
 	M							= glm::translate(M, glm::vec3(0.0f, magic, 0.0f));
 	M							= glm::rotate(M, glm::radians(90.0f), glm::vec3(1.0f, 0.0f, 0.0f));
 	M							= glm::translate(M, glm::vec3(0.0f, 0.0f, thickness / 2.0f - this->floor / 2.0f));
@@ -66,7 +67,7 @@ void GameBoard::draw(GLFWwindow *window, glm::mat4 P, glm::mat4 V) {
 	this->modelWall->texRepeat = glm::vec2(this->length / 5.0f, (this->height + this->floor / 2.0f) / 5.0f);
 
 	// wall Z+ outer
-	M = glm::mat4(1.0f);
+	M = glm::translate(glm::mat4(1.0f), offset);
 	M = glm::translate(M, glm::vec3(0.0f, 0.0f, magic));
 	M = glm::rotate(M, glm::radians(180.0f), glm::vec3(0.0f, 1.0f, 0.0f));
 	M = glm::translate(
@@ -76,7 +77,7 @@ void GameBoard::draw(GLFWwindow *window, glm::mat4 P, glm::mat4 V) {
 	M = glm::scale(M, glm::vec3(this->length + magic * 2.0f, this->height + this->floor / 2.0f, thickness));
 	this->modelWall->draw(window, ShaderProgramType::SP_TBN, P, V, M);
 	// wall Z- outer
-	M = glm::mat4(1.0f);
+	M = glm::translate(glm::mat4(1.0f), offset);
 	M = glm::translate(M, glm::vec3(0.0f, 0.0f, -magic));
 	M = glm::translate(
 		M,
@@ -86,29 +87,35 @@ void GameBoard::draw(GLFWwindow *window, glm::mat4 P, glm::mat4 V) {
 	this->modelWall->draw(window, ShaderProgramType::SP_TBN, P, V, M);
 
 	// wall Z+ inner
-	M = glm::mat4(1.0f);
+	M = glm::translate(glm::mat4(1.0f), offset);
 	M = glm::translate(M, glm::vec3(0.0f, 0.0f, -magic));
 	M = glm::translate(
 		M,
 		glm::vec3(0.0f, this->height / 2.0f - this->floor / 4.0f, thickness / 2.0f + this->width / 2.0f - this->wall)
 	);
-	M = glm::scale(M, glm::vec3(this->length, this->height + this->floor / 2.0f, thickness));
+	M = glm::scale(
+		M,
+		glm::vec3(this->length - this->wall * 2.0f - magic * 2.0f, this->height + this->floor / 2.0f, thickness)
+	);
 	this->modelWall->draw(window, ShaderProgramType::SP_TBN, P, V, M);
 	// wall Z- inner
-	M = glm::mat4(1.0f);
+	M = glm::translate(glm::mat4(1.0f), offset);
 	M = glm::translate(M, glm::vec3(0.0f, 0.0f, magic));
 	M = glm::rotate(M, glm::radians(180.0f), glm::vec3(0.0f, 1.0f, 0.0f));
 	M = glm::translate(
 		M,
 		glm::vec3(0.0f, this->height / 2.0f - this->floor / 4.0f, thickness / 2.0f + this->width / 2.0f - this->wall)
 	);
-	M = glm::scale(M, glm::vec3(this->length, this->height + this->floor / 2.0f, thickness));
+	M = glm::scale(
+		M,
+		glm::vec3(this->length - this->wall * 2.0f - magic * 2.0f, this->height + this->floor / 2.0f, thickness)
+	);
 	this->modelWall->draw(window, ShaderProgramType::SP_TBN, P, V, M);
 
 	this->modelWall->texRepeat = glm::vec2(this->width / 5.0f, (this->height + this->floor / 2.0f) / 5.0f);
 
 	// wall X+ outer
-	M = glm::mat4(1.0f);
+	M = glm::translate(glm::mat4(1.0f), offset);
 	M = glm::translate(M, glm::vec3(-magic, 0.0f, 0.0f));
 	M = glm::rotate(M, glm::radians(90.0f), glm::vec3(0.0f, 1.0f, 0.0f));
 	M = glm::translate(
@@ -118,7 +125,7 @@ void GameBoard::draw(GLFWwindow *window, glm::mat4 P, glm::mat4 V) {
 	M = glm::scale(M, glm::vec3(this->width + magic * 2.0f, this->height + this->floor / 2.0f, thickness));
 	this->modelWall->draw(window, ShaderProgramType::SP_TBN, P, V, M);
 	// wall X- outer
-	M = glm::mat4(1.0f);
+	M = glm::translate(glm::mat4(1.0f), offset);
 	M = glm::translate(M, glm::vec3(magic, 0.0f, 0.0f));
 	M = glm::rotate(M, glm::radians(-90.0f), glm::vec3(0.0f, 1.0f, 0.0f));
 	M = glm::translate(
@@ -129,24 +136,30 @@ void GameBoard::draw(GLFWwindow *window, glm::mat4 P, glm::mat4 V) {
 	this->modelWall->draw(window, ShaderProgramType::SP_TBN, P, V, M);
 
 	// wall X+ inner
-	M = glm::mat4(1.0f);
+	M = glm::translate(glm::mat4(1.0f), offset);
 	M = glm::translate(M, glm::vec3(magic, 0.0f, 0.0f));
 	M = glm::rotate(M, glm::radians(-90.0f), glm::vec3(0.0f, 1.0f, 0.0f));
 	M = glm::translate(
 		M,
 		glm::vec3(0.0f, this->height / 2.0f - this->floor / 4.0f, thickness / 2.0f + this->length / 2.0f - this->wall)
 	);
-	M = glm::scale(M, glm::vec3(this->width, this->height + this->floor / 2.0f, thickness));
+	M = glm::scale(
+		M,
+		glm::vec3(this->width - this->wall * 2.0f - magic * 2.0f, this->height + this->floor / 2.0f, thickness)
+	);
 	this->modelWall->draw(window, ShaderProgramType::SP_TBN, P, V, M);
 	// wall X- inner
-	M = glm::mat4(1.0f);
+	M = glm::translate(glm::mat4(1.0f), offset);
 	M = glm::translate(M, glm::vec3(-magic, 0.0f, 0.0f));
 	M = glm::rotate(M, glm::radians(90.0f), glm::vec3(0.0f, 1.0f, 0.0f));
 	M = glm::translate(
 		M,
 		glm::vec3(0.0f, this->height / 2.0f - this->floor / 4.0f, thickness / 2.0f + this->length / 2.0f - this->wall)
 	);
-	M = glm::scale(M, glm::vec3(this->width, this->height + this->floor / 2.0f, thickness));
+	M = glm::scale(
+		M,
+		glm::vec3(this->width - this->wall * 2.0f - magic * 2.0f, this->height + this->floor / 2.0f, thickness)
+	);
 	this->modelWall->draw(window, ShaderProgramType::SP_TBN, P, V, M);
 }
 
